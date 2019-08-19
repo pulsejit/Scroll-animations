04a8332656e4c3f53f284e3e12140da7ce27a03e
Subhajit Chatterjee <subhajitchatterjee425@gmail.com>
2019-08-19 12:45:47 +0530

    looping function added only the responsive image is left and img reso problem unsplash isn't working

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 
 var controller = new ScrollMagic.Controller();
+
+
+$(".project").each(function(){
+
+ var picOverlay = $(this).find(".overlay");
+ var projectInfo = $(this).find(".project-info");
+ var samllTitle = $(this).find(".small-title");
+ var projectLink = $(this).find(".project-link");
+ var title = $(this).find("h4");
+
 var animateIn = new TimelineMax();
 
 animateIn
-.fromTo(".overlay", 2, {skewX:30, scale:1.5}, {skewX:0, xPercent:100, tranfromOrigin: "0% 100%", ease: Power4.easeOut})
+.fromTo(picOverlay, 2, {skewX:30, scale:1.5}, {skewX:0, xPercent:100, tranfromOrigin: "0% 100%", ease: Power4.easeOut})
 
-.from(".project-info", 1, {scaleY:0, transformOrigin: "bottom left"}, "-=1.5")
+.from(projectInfo, 1, {scaleY:0, transformOrigin: "bottom left"}, "-=1.5")
 
-.from("h4", .3,{autoAlpha: 0, y:30, ease:Power4.easeOut}, "-=.7")
+.from(title, .3,{autoAlpha: 0, y:30, ease:Power4.easeOut}, "-=.7")
 
-.from(".small-title", .3, {autoAlpha: 0, y:30, ease:Power4.easeOut}, "-=.7")
+.from(samllTitle, .3, {autoAlpha: 0, y:30, ease:Power4.easeOut}, "-=.7")
 
-.from(".project-link", .7, {autoAlpha: 0, y:30, ease:Power4.easeOut}, "-=.7")
+.from(projectLink, .7, {autoAlpha: 0, y:30, ease:Power4.easeOut}, "-=.7")
 
 // make a scroll magic scene
 
 var scene = new ScrollMagic.Scene({
-    triggerElement: ".project"
+    //Scene is a constractor so the first letter will be in clps also same goes for the constractor also
+    triggerElement: this,
 })
 
 .setTween(animateIn).addTo(controller)
+})
